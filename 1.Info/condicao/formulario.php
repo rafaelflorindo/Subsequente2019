9dd4f3a96f233d1732c75d8cd8db8235af5b847e
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-05-30 22:11:42 -0300

    Add files via upload

---
+++ 1.Info/condicao/formulario.php
@@ -0,0 +1,9 @@
+<form action = "verificar.php" method="post">
+	<label>Número 1</label>
+	<input type="number" name="num1">
+
+	<label>Número 2</label>
+	<input type="number" name="num2">
+
+	<input type="submit" value="Verificar">
+</form>
